f36ae37cc46613f9ff73777ac3b6378253f0141f
svc-teamcity-agent <svc-teamcity-agent@svc-teamsity-agent.local>
2023-02-10 12:05:17 +0300

    Release 1.0.57

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrackingKit",
-            url: "https://mmr.jfrog.io/artifactory/rs-tracking-sdk-ios/1.0.56/TrackingKit.xcframework.zip",
-            checksum: "bb458a3b74a04360d30e3617e6c336d6411115dbd0eb1e2307ef97265fadc7ad"
+            url: "https://mmr.jfrog.io/artifactory/rs-tracking-sdk-ios/1.0.57/TrackingKit.xcframework.zip",
+            checksum: "0cbaee20f38b21e1ac9adc5ca19e1e8eb8f1acebafbec454473780edd3555adc"
         )
     ]
 )
